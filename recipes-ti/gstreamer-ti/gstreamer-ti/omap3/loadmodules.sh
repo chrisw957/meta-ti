2567dc41c18bb389debc5e45b5f4e96ec705d609
Chris Whittenburg <whittenburg@gmail.com>
2015-05-26 14:44:44 -0500

    Modify loadmodules.sh to work with 720P h264 on dm3730

--- recipes-ti/gstreamer-ti/gstreamer-ti/omap3/loadmodules.sh
+++ recipes-ti/gstreamer-ti/gstreamer-ti/omap3/loadmodules.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 #
-# CMEM Allocation
-#    1x5250000            Circular buffer
-#    6x829440,1x691200    Video buffers (max D1 PAL)
-#    1x345600             Underlying software components (codecs, etc.)
-#    1x1                  Dummy buffer used during final flush
+# This version has been modified to work with 720P H.264 encoding.
+#
 
 rmmod cmemk 2>/dev/null
 
-modprobe cmemk allowOverlap=1 phys_start=0x86300000 phys_end=0x87300000 \
-        pools=1x5250000,6x829440,1x345600,1x691200,1x1
+modprobe cmemk allowOverlap=1 phys_start=0x83700000 phys_end=0x85900000 \
+     pools=2x5531400,3x2457600,2x921600,1x345600,1x691200,1x1
 
 # insert DSP/BIOS Link driver
 modprobe dsplinkk
